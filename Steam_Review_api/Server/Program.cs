6a2f139820d9079f8b53b98042ca813852ac1f8e
Sean-mn <b01080080329@gmail.com>
2025-06-18 02:03:48 +0900

    feat: 포트 바인딩 추가

--- Steam_Review_api/Server/Program.cs
+++ Steam_Review_api/Server/Program.cs
@@ -6,6 +6,9 @@ using Server.Service.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddScoped<IGameReviewRepository, GameReviewRepository>();
 builder.Services.AddScoped<ICsvService, CsvService>();
 builder.Services.AddScoped<IGameReviewService, GameReviewService>();
