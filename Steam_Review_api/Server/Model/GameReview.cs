c34cd52b0911cdee86062becfffed17b67bc739c
Sean-mn <b01080080329@gmail.com>
2025-06-17 11:27:13 +0900

    feat: 게임 리뷰 모델 추가

---
+++ Steam_Review_api/Server/Model/GameReview.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Model;
+
+public class GameReview
+{
+    public int ReviewId { get; set; }
+    public string Game { get; set; }
+    public int Year { get; set; }
+    public string Review { get; set; }
+    public string Sentiment { get; set; }
+    public string Language { get; set; }
+}
