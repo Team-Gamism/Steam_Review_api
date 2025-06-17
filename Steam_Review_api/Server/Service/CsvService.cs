ef5cacb0e86e53e0c7797f9473ee6d6275be2d25
Sean-mn <b01080080329@gmail.com>
2025-06-17 12:07:37 +0900

    feat: CsvService 구현

---
+++ Steam_Review_api/Server/Service/CsvService.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using CsvHelper;
+using Server.Model;
+using Server.Service.Interface;
+
+namespace Server.Service;
+
+public class CsvService :  ICsvService
+{
+    public IEnumerable<GameReview> ReadAll(string path)
+    {
+        using var reader = new StreamReader(path);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        
+        var records = csv.GetRecords<GameReview>().ToList();
+        
+        return records;
+    }
+}
---
+++ Steam_Review_api/Server/Service/Interface/ICsvService.cs
@@ -0,0 +1,8 @@
+﻿using Server.Model;
+
+namespace Server.Service.Interface;
+
+public interface ICsvService
+{
+    IEnumerable<GameReview> ReadAll(string path);
+}
